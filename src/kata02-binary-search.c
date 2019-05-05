fe19deeeb78ecdbb2a660ed8674e646599d4043e
James Little <alphafoobar@users.noreply.github.com>
2019-05-05 22:36:21 +1200

    Let length == 1 pass into recursion

--- src/kata02-binary-search.c
+++ src/kata02-binary-search.c
@@ -1,16 +1,10 @@
 int chop(int key, int *array, int offset, int length) {
     const int remaining = length - offset;
-
     if (remaining == 0) {
         return -1;
     }
-    if (remaining == 1) {
-        return array[offset] == key ? offset : -1;
-    }
 
-    int mid_point = remaining / 2;
-
-    int point = offset + mid_point;
+    int point = offset + remaining / 2;
     if (array[point] == key) {
         return point;
     }
