365a592fd204abdf2e02ccbd670e09ad17e210e9
James Little <alphafoobar@users.noreply.github.com>
2019-08-06 00:38:35 +1200

    Integrated shellcheck suggestions
    See https://github.com/koalaman/shellcheck/wiki/SC2155

--- pre-install.sh
+++ pre-install.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-export ROOT_WORKING_DIR=$(pwd)/build-dependencies
+ROOT_WORKING_DIR=$(pwd)/build-dependencies
+export ROOT_WORKING_DIR
 
-cd ${ROOT_WORKING_DIR}/cmocka-1.1.5
+cd "${ROOT_WORKING_DIR}/cmocka-1.1.5" || exit
 
 rm -rf build && \
     mkdir -p build && \
     cd build && \
     cmake \
-        -DCMAKE_INSTALL_PREFIX=${ROOT_WORKING_DIR}/install/cmocka-1.1.5 \
+        -DCMAKE_INSTALL_PREFIX="${ROOT_WORKING_DIR}/install/cmocka-1.1.5" \
         -DCMAKE_BUILD_TYPE=Debug .. && \
     make install
