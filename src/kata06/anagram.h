9ef442ae83dc19fbcda911e3ac2ea9c04e14382b
James Little <alphafoobar@users.noreply.github.com>
2019-08-16 01:24:34 +1200

    Added placeholder for anagram

---
+++ src/kata06/anagram.h
@@ -0,0 +1,4 @@
+#ifndef C_ANAGRAM_H
+#define C_ANAGRAM_H
+
+#endif //C_ANAGRAM_H
---
+++ tests/kata06/anagram_test.c
@@ -0,0 +1,8 @@
+#include <stdarg.h>
+#include <stddef.h>
+// setjmp required
+#include <setjmp.h>
+
+#include "kata06/anagram.h"
+
+#include <cmocka.h>
